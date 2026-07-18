3187ddaf3548a1b0d3945927e582c9befebdd649
Chr1ztt <chilllikehuman@gmail.com>
2026-07-18 09:29:34 +0700

    fix; resolve animation path, now start from mid

--- src/features/landing/components/profile-photo.tsx
+++ src/features/landing/components/profile-photo.tsx
@@ -3,27 +3,35 @@
 import { gsap, useGSAP } from "@/lib/gsap"
 
 export default function ProfilePhoto() {
-  const leftFloatShapeClass = "left-float-shape"
-  const rightFloatShapeClass = "right-float-shape"
+  const floatShapeClass = "float-shape"
   useGSAP(() => {
-    gsap.utils.toArray(`.${leftFloatShapeClass}`).forEach((el, i) => {
-      const distance = 10 + Math.random() * 8 // naik-turun 10-18px
-      const duration = 2.2 + Math.random() * 1.4 // 2.2 - 3.6 detik per siklus
-      const rotateAmount = (Math.random() > 0.5 ? 1 : -1) * (1 + Math.random() * 2)
+    gsap.utils.toArray(`.${floatShapeClass}`).forEach((el, i) => {
+      const distance = 20 + Math.random() * 8
+      const duration = 2.5 + Math.random() * 1.0
+      // const rotateAmount = (Math.random() > 0.5 ? 1 : -1) * (1 + Math.random() * 2)
+      const rightDirection = Math.random() > 0.5
+      const delay = Math.random() * 3
 
-      gsap.to(el, {
-        x: `+=${distance*2}`,
-        y: `-=${distance}`,
-        rotation: rotateAmount,
-        // svgOrigin agar GSAP pakai SVG attribute transform, bukan CSS transform
-        // sehingga animasi path di dalam clipPath bisa berjalan di semua browser
-        svgOrigin: "150 200",
-        duration: duration,
-        repeat: -1,
-        yoyo: true,
-        ease: "sine.inOut",
-        delay: i * 0.25, // efek berjenjang, shape tidak bergerak serentak
-      })
+      gsap.fromTo(
+        el as gsap.TweenTarget,
+        {
+          x: `${rightDirection ? "-" : "+"}=${distance * 1.975}`,
+          y: `${rightDirection ? "+" : "-"}=${distance}`,
+        },
+        {
+          x: `${rightDirection ? "+" : "-"}=${distance * 1.975 * 2}`,
+          y: `${rightDirection ? "-" : "+"}=${distance * 2}`,
+          // rotation: rotateAmount,
+          // svgOrigin agar GSAP pakai SVG attribute transform, bukan CSS transform
+          // sehingga animasi path di dalam clipPath bisa berjalan di semua browser
+          svgOrigin: "150 200",
+          duration: duration,
+          repeat: -1,
+          yoyo: true,
+          ease: "sine.inOut",
+          delay: delay, // efek berjenjang, shape tidak bergerak serentak
+        }
+      )
     })
   })
   return (
@@ -35,6 +43,7 @@ export default function ProfilePhoto() {
         fill="#F5F5F5"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
+        overflow="visible"
       >
         <mask
           id="mask0_91_93"
@@ -52,80 +61,193 @@ export default function ProfilePhoto() {
           />
         </mask>
         <clipPath id="shape1">
-          <path className={leftFloatShapeClass} d="M 48.2227 374.659 l 207.1823 -104.91 v 25.341 l -207.1823 104.91 v -25.341 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 48.2227 374.659 l 207.1823 -104.91 v 25.341 l -207.1823 104.91 v -25.341 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape2">
-          <path className={leftFloatShapeClass} d="M 164.994 333.55 l 71.622 -36.267 v 17.767 l -71.622 36.267 v -17.767 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 164.994 333.55 l 71.622 -36.267 v 17.767 l -71.622 36.267 v -17.767 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape3">
-          <path className={leftFloatShapeClass} d="M 153.362 370.398 l 134.896 -68.307 v 29.602 l -134.896 68.307 v -29.602 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 153.362 370.398 l 134.896 -68.307 v 29.602 l -134.896 68.307 v -29.602 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape4">
-          <path className={leftFloatShapeClass} d="M 133.333 287.29 l 91.389 -46.276 v 19.822 l -91.389 46.276 v -19.822 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 133.333 287.29 l 91.389 -46.276 v 19.822 l -91.389 46.276 v -19.822 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape5">
           <path
-            className={leftFloatShapeClass}
+            className={floatShapeClass}
             d="M 61.7115 343.379 l 163.0105 -82.544 l 19.82 9.317 l -163.01 82.544 l -19.812 -9.317 Z"
             fill="#F5F5F5"
           />
         </clipPath>
         <clipPath id="shape6">
-          <path className={leftFloatShapeClass} d="M 0 343.379 l 101.107 -51.197 l 25.92 9.796 l -101.107 51.197 l -25.92 -9.796 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 0 343.379 l 101.107 -51.197 l 25.92 9.796 l -101.107 51.197 l -25.92 -9.796 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape7">
-          <path className={leftFloatShapeClass} d="M 27.3017 308.522 l 158.8473 -80.435 v 34.857 l -158.847 80.435 v -34.857 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 27.3017 308.522 l 158.8473 -80.435 v 34.857 l -158.847 80.435 v -34.857 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape8">
-          <path className={leftFloatShapeClass} d="M 175.724 193.953 l 124.276 -62.93 v 27.271 l -124.276 62.93 v -27.271 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 175.724 193.953 l 124.276 -62.93 v 27.271 l -124.276 62.93 v -27.271 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape9">
-          <path className={leftFloatShapeClass} d="M 52.7024 237.322 l 201.041 -101.801 l 0 29.222 l -201.041 101.801 l 0 -29.222 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 52.7024 237.322 l 201.041 -101.801 l 0 29.222 l -201.041 101.801 l 0 -29.222 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape10">
           <path
-            className={leftFloatShapeClass}
+            className={floatShapeClass}
             d="M 61.8729 266.935 l 85.532 -43.31 l 21.927 8.287 l -85.532 43.31 l -21.927 -8.287 Z"
             fill="#F5F5F5"
           />
         </clipPath>
         <clipPath id="shape11">
-          <path className={leftFloatShapeClass} d="M 190.873 219.097 l 67.187 -34.022 v 25.988 l -67.187 34.022 v -25.988 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 190.873 219.097 l 67.187 -34.022 v 25.988 l -67.187 34.022 v -25.988 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape12">
           <path
-            className={leftFloatShapeClass}
+            className={floatShapeClass}
             d="M 47.6479 200.564 l 139.336 -70.556 l 35.721 13.5 l -139.336 70.556 l -35.721 -13.5 Z"
             fill="#F5F5F5"
           />
         </clipPath>
         <clipPath id="shape13">
-          <path className={leftFloatShapeClass} d="M 119.571 142.495 l 155.654 -78.818 v 34.156 l -155.654 78.818 v -34.156 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 119.571 142.495 l 155.654 -78.818 v 34.156 l -155.654 78.818 v -34.156 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape14">
-          <path className={leftFloatShapeClass} d="M 38.4306 166.95 l 78.277 -39.637 v 30.277 l -78.277 39.637 v -30.277 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 38.4306 166.95 l 78.277 -39.637 v 30.277 l -78.277 39.637 v -30.277 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape15">
-          <path className={leftFloatShapeClass} d="M 119.369 119.459 l 134.375 -68.043 v 29.145 l -134.375 68.043 v -29.145 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 119.369 119.459 l 134.375 -68.043 v 29.145 l -134.375 68.043 v -29.145 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape16">
-          <path className={leftFloatShapeClass} d="M 119.484 93.9354 l 108.753 -55.065 v 29.226 l -108.753 55.065 v -29.226 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 119.484 93.9354 l 108.753 -55.065 v 29.226 l -108.753 55.065 v -29.226 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape17">
           <path
-            className={leftFloatShapeClass}
+            className={floatShapeClass}
             d="M 0.0648 170.018 l 90.108 -45.628 l 23.1008 8.73 l -90.108 45.628 l -23.1008 -8.73 Z"
             fill="#F5F5F5"
           />
         </clipPath>
         <clipPath id="shape18">
-          <path className={leftFloatShapeClass} d="M 29.2773 101.801 l 201.041 -101.8 v 29.222 l -201.041 101.8 v -29.222 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 29.2773 101.801 l 201.041 -101.8 v 29.222 l -201.041 101.8 v -29.222 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape19">
-          <path className={leftFloatShapeClass} d="M 29.0161 131.023 l 61 -31 l 22.1 7.28 l -61 31 l -22.1 -7.28 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 29.0161 131.023 l 61 -30.886 l 22.1 7.28 l -61 30.886 l -22.1 -7.28 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
         <clipPath id="shape20">
-          <path className={leftFloatShapeClass} d="M 14.8147 68.1926 l 103.7 -52.65 l 37.6 12.33 l -103.7 52.65 l -37.6 -12.33 Z" fill="#F5F5F5" />
+          <path
+            className={floatShapeClass}
+            d="M 14.8147 68.1926 l 103.7 -52.5063 l 37.6 12.33 l -103.7 52.5063 l -37.6 -12.33 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape21">
+          <path
+            className={floatShapeClass}
+            d="M 4.6416 246.289 l 113.6324 -57.539 l 29.132 11.009 l -113.6321 57.539 l -29.132 -11.009 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape22">
+          <path
+            className={floatShapeClass}
+            d="M 194.894 146.535 l 90.388 -45.77 v 11.056 l -90.388 45.77 v -11.055 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape23">
+          <path
+            className={floatShapeClass}
+            d="M 33.7729 295.09 l 95.286 -48.2461 v 13.85 l -95.286 48.2461 v -13.85 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape24">
+          <path
+            className={floatShapeClass}
+            d="M 118.865 225.503 l 106.708 -54.034 v 15.51 l -106.708 54.034 v -15.51 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape25">
+          <path
+            className={floatShapeClass}
+            d="M 87.128 304.166 l 150.245 -76.079 l 38.518 14.556 l -150.245 76.079 l -38.518 -14.556 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape26">
+          <path
+            className={floatShapeClass}
+            d="M 56.0691 391.088 l 74.7 -37.8228 l 27.1 8.9 l -74.7 37.8228 l -27.1 -8.9 Z"
+            fill="#F5F5F5"
+          />
+        </clipPath>
+        <clipPath id="shape27">
+          <path
+            className={floatShapeClass}
+            d="M 23.372 162.368 l 158.847 -80.436 l 0 34.858 l -158.847 80.436 l 0 -34.858 Z"
+            fill="#F5F5F5"
+          />
         </clipPath>
 
         {/* <rect fill="url(#pattern0_91_93)"/> */}
@@ -189,6 +311,27 @@ export default function ProfilePhoto() {
         <g clipPath="url(#shape20)">
           <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
         </g>
+        <g clipPath="url(#shape21)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape22)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape23)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape24)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape25)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape26)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
+        <g clipPath="url(#shape27)">
+          <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
+        </g>
 
         {/* <g mask="url(#mask0_91_93)">
           <rect x="-97.4883" y="-36.3908" width="497.754" height="662.809" fill="url(#pattern0_133_139)" />
@@ -223,7 +366,7 @@ export default function ProfilePhoto() {
 // }
 
 // {
-  /* <defs>
+/* <defs>
             <pattern id="pattern0_91_93" patternContentUnits="objectBoundingBox" width="1" height="1">
               <use xlinkHref="#image0_91_93" transform="matrix(0.000325357 0 0 0.000244141 -0.000398697 0)" />
             </pattern>
